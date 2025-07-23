b21fa9d876e740d5f97c8e067d0f931c011d4190
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-07-23 07:09:12 +0300

    updated

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,4 +6,17 @@ const app = Fastify({ logger: true }).withTypeProvider<TypeBoxTypeProvider>();
 
 app.register(healthRoutes);
 
+// Start server
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+    console.log("Server running at http://localhost:3000");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
+
 export default app;
