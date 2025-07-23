d6032dde919d190e8f21101784b41c57f5ba1c52
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-07-23 14:10:33 +0300

    updated

--- server/tests/health.test.ts
+++ server/tests/health.test.ts
@@ -1,6 +1,9 @@
 import request from "supertest";
 import app from "../src/app";
 
+// If using TypeScript, add the following for Jest types:
+import { describe, beforeAll, afterAll, it, expect } from "@jest/globals";
+
 describe("Health Endpoint", () => {
   let server: any;
 
