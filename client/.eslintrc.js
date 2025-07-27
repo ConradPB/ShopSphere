14987e6617850fbd0d31b7644c525fd2d3088e36
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-07-27 22:40:10 +0300

    updated

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   env: { browser: true, es2020: true, node: true },
-  extends: ['next', 'next/core-web-vitals'],
+  extends: ['next', 'next/core-web-vitals', 'eslint:recommended'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off' // Optional, if prop-types errors appear
+    'react/prop-types': 'off',
+    'no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
   },
 };
