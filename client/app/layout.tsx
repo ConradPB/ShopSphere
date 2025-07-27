3534f3a394171884b372ea289680054f2b719c7b
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-07-28 00:15:29 +0300

    updated

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-import type { Metadata } from "next";
-import "./globals.css";
-import type { FC } from "react";
+import type { Metadata } from 'next';
+import './globals.css';
+import type { FC } from 'react';
 
 export const metadata: Metadata = {
-  title: "ShopSphere",
-  description: "A stunning e-commerce platform",
+  title: 'ShopSphere',
+  description: 'A stunning e-commerce platform',
 };
 
 const RootLayout: FC<{ children: React.ReactNode }> = ({ children }) => {
