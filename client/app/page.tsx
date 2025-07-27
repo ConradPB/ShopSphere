e9395f2d32d4fe88433a8dd48a3a4d49b6bb0625
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-07-27 22:38:59 +0300

    updated

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,7 @@
-import type { FC } from "react";
+import type { FC } from 'react';
 
 const Home: FC = () => {
-  return (
-    <h1 className="text-4xl font-bold text-center mt-8">
-      Welcome to ShopSphere
-    </h1>
-  );
+  return <h1 className="text-4xl font-bold text-center mt-8">Welcome to ShopSphere</h1>;
 };
 
 export default Home;
