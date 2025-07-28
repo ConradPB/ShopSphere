562e76e174f9c2e612d48a2f617929810af1ac97
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-07-29 00:20:58 +0300

    updated

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,13 @@
 import type { FC } from "react";
 import { supabase } from "@/lib/supabase";
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image_url: string;
+}
+
 const Home: FC = async () => {
   const { data: products, error } = await supabase.from("products").select("*");
 
