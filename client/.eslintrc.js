72d6fa184c537effdf1ec005021754d1398691ea
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-07-29 07:04:04 +0300

    removed/outdated

--- client/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: { browser: true, es2020: true, node: true },
-  extends: ['next', 'next/core-web-vitals', 'eslint:recommended'],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
-  },
-};
