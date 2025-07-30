18d6162314f1e1f027392ba7f38c86b9020fafa0
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-07-30 23:51:43 +0300

    updated

--- client/lib/test-supabase.ts
+++ client/lib/test-supabase.ts
@@ -2,6 +2,7 @@ import { supabase } from "./supabase";
 
 async function test() {
   const { data, error } = await supabase.from("products").select("*");
+  console.log("Data:", data, "Error:", error);
 }
 
 test();
