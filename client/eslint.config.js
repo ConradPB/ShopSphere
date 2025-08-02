2c06218c67f766d51a027bc76623936a1a55eb0f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-08-02 23:58:20 +0300

    updated

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,62 +1,7 @@
-// eslint.config.js
+// @ts-check
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import reactPlugin from 'eslint-plugin-react';
 import hooksPlugin from 'eslint-plugin-react-hooks';
 import nextPlugin from '@next/eslint-plugin-next';
 
-export default [
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    plugins: {
-      react: reactPlugin,
-      'react-hooks': hooksPlugin,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-    },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    plugins: {
-      '@next/next': nextPlugin,
-    },
-    rules: {
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      '@next/next/no-html-link-for-pages': 'error',
-      '@next/next/no-sync-scripts': 'error',
-      '@next/next/google-font-display': 'off',
-      '@next/next/no-img-element': 'warn',
-    },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    ignores: ['.next/**', 'node_modules/**', 'dist/**', 'build/**'],
-    languageOptions: {
-      parser: tseslint.parser,
-      sourceType: 'module',
-      globals: {
-        browser: true,
-        es2020: true,
-        node: true,
-        React: 'writable',
-        console: 'readonly',
-        process: 'readonly',
-      },
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: '^_' }],
-    },
-  },
-];
