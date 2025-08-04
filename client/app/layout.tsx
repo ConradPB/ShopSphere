8293a6c42377e4ee9daba93e516dcec2e82dc08b
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-08-04 23:51:48 +0300

    udpated

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const robo = Roboto({ subsets: ["latin"], display: "swap" });
+const roboto = Roboto({ subsets: ["latin"], display: "swap" });
 
 const RootLayout: FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
