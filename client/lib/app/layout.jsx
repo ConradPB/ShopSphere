fd39864396611474b901bb5d74cc8fa63d2d383f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-08-06 16:29:16 +0300

    deleted

--- client/lib/app/layout.jsx
+++
@@ -1,9 +0,0 @@
-import { Roboto } from "next/font/google";
-import "./globals.css";
-const roboto = Roboto({ subsets: ["latin"], display: "swap" });
-const RootLayout = ({ children }) => {
-    return (<html lang="en" className={roboto.className}>
-      <body>{children}</body>
-    </html>);
-};
-export default RootLayout;
