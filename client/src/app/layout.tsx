d2ec46bba65dadd4eac6101b9827cb4dedba6e32
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-08-12 15:51:40 +0300

    updated

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navbar from "@/components/Navbar"; // 👈 Import the navbar
+import Navbar from "@/components/Navbar";
+import { CartProvider } from "@/context/CartContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
