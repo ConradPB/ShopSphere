b69547d892cd39c9ab23950eeb3ba9a76938f6ae
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-08-30 03:02:33 +0300

    updated

--- client/src/components/CartDebugger.tsx
+++ client/src/components/CartDebugger.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import React from "react";
 import { useAppSelector } from "@/redux/hooks";
 
 export default function CartDebugger() {
-  const items = useAppSelector((state) => state.cart.items);
+  const items = useAppSelector((s) => s.cart.items);
 
   return (
-    <div className="fixed bottom-4 right-4 bg-white border p-4 shadow-lg rounded max-w-xs">
+    <div className="fixed bottom-4 right-4 bg-white border p-4 shadow-lg rounded max-w-xs z-50">
       <h2 className="font-bold mb-2">Cart Debugger</h2>
       <pre className="text-xs overflow-auto max-h-40">
         {JSON.stringify(items, null, 2)}
