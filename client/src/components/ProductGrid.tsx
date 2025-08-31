170754c848fa385fb25a7fd197905b81351920af
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-08-31 23:55:35 +0300

    updated

--- client/src/components/ProductGrid.tsx
+++ client/src/components/ProductGrid.tsx
@@ -6,7 +6,7 @@ import { Product } from "@/types/product";
 import ProductCard from "./ProductCard";
 
 export default function ProductGrid() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Prduct[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
