c593244dfba3f09f7f8fa1094ec979f469a18c8f
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-09-03 20:49:29 +0300

    updated

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -11,8 +11,6 @@ export default async function HomePage() {
   }
 
   return (
-
-    <SearchAndFilterClient initialProducts={products ?? []} />;
     <div className="max-w-6xl mx-auto p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {products?.map((product) => (
         <Link
