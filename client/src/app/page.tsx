295c8b60e8e8e4cbb032991daa45d992eb84b533
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-09-06 22:44:45 +0300

    updated

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-700 via-pink-600 to-red-500 text-white text-5xl font-bold">
-      Tailwind Works!
+    <div className="min-h-screen flex items-center justify-center bg-primary text-white text-4xl font-display">
+      Tailwind & custom theme working!
     </div>
   );
 }
