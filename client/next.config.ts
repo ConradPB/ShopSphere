f5d49a083b9cccc03a173e7a9370382e5201f879
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-09-06 23:24:32 +0300

    applied csp to all routes

--- client/next.config.ts
+++ client/next.config.ts
@@ -9,6 +9,22 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // apply CSP to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              process.env.NODE_ENV === "development"
+                ? "default-src 'self'; script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
+                : "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
