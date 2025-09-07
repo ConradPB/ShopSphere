df1e9689c9fe276a78bce4d9390918dbdcfced06
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-09-07 23:25:27 +0300

    updated

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -7,4 +7,4 @@ import "@testing-library/jest-dom";
 process.env.NEXT_PUBLIC_SUPABASE_URL =
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? "http://localhost";
 process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "anon
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "anon";
