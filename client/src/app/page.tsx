747d76c007e3bebfb45a748b5c9cca408575c5bc
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-09-12 15:52:11 +0300

    updated

--- client/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center gap-6">
-      <h1>🚀 Tailwind v4 is really working!</h1>
-      <button>Click Me</button>
-    </main>
-  );
-}
