29dd38bb870eb37da77dbc0978b44f983745673c
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-09-12 15:49:20 +0300

    updated

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -9,16 +9,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className="min-h-screen bg-blue-50 text-gray-900 flex items-center justify-center">
-        <ReduxProvider>
-          <div className="w-full max-w-md p-6 bg-white shadow-xl rounded-xl">
-            {children}
-            {ENABLE_CART_DEBUG ? <CartDebugger /> : null}
-          </div>
-        </ReduxProvider>
-      </body>
-    </html>
-  );
+  return <html lang="en"></html>;
 }
