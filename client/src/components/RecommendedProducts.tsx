0cd55d3eb8e77149501229ac0d82694ad94e08ec
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-09-16 23:48:53 +0300

    updated

--- client/src/components/RecommendedProducts.tsx
+++ client/src/components/RecommendedProducts.tsx
@@ -31,7 +31,7 @@ export default function RecommendedProducts({
                        33vw"
                 placeholder="blur"
                 blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                  shimmer(700, 475)
+                  shimer(700, 475)
                 )}`}
                 unoptimized
               />
