0f8182a6d97ebd4d80df3a4f3a6b589457cf8f0c
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-09-23 23:44:12 +0300

    updated

--- client/src/components/ProductDetailClient.tsx
+++ client/src/components/ProductDetailClient.tsx
@@ -117,7 +117,7 @@ export default function ProductDetailClient({
                     <div className="p-2">
                       <h4 className="text-sm font-medium truncate">
                         {r.title}
-                      </h
+                      </h4>
                       <p className="text-xs text-indigo-600">
                         ${r.price.toFixed(2)}
                       </p>
