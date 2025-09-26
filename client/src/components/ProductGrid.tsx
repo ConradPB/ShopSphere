5a8dfcbeb70e894c6a5bb32c93cc736f86697bf4
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-09-26 09:27:14 +0300

    improved grid

--- client/src/components/ProductGrid.tsx
+++ client/src/components/ProductGrid.tsx
@@ -62,7 +62,7 @@ export default function ProductGrid({ initialProducts, title }: Props) {
         </h2>
       )}
 
-      <div className="">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {list.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
