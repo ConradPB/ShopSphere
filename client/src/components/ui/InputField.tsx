8495dc6814202819ebb17757c835dc3fc22cb6b3
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-09-28 16:14:59 +0300

    updated

---
+++ client/src/components/ui/InputField.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+
+interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
+
+export default function InputField({ label, ...props }: InputFieldProps) {
+  return (
+    <div className="flex flex-col space-y-1">
+      {label && (
+        <label className="text-sm font-medium text-gray-700">{label}</label>
+      )}
+      <input
+        {...props}
+        className="border border-gray-300 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
+      />
+    </div>
+  );
+}
