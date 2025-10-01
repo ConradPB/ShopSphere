d766142528e4c474af19236d71122af178488c37
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-10-01 05:24:56 +0300

    updated

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,9 +1,4 @@
 import "./globals.css";
-import { ReduxProvider } from "@/redux/Provider";
-import CartDebugger from "@/components/CartDebugger";
-import Navbar from "@/components/Navbar";
-
-const ENABLE_CART_DEBUG = process.env.NEXT_PUBLIC_ENABLE_CART_DEBUG === "true";
 
 export default function RootLayout({
   children,
@@ -12,16 +7,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-neutral-50 text-neutral-900 font-sans antialiased">
-        <ReduxProvider>
-          <Navbar />
-          {/* Global wrapper */}
-          <main className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
-            {/* add top padding to account for fixed navbar */}
-            {children}
-            {ENABLE_CART_DEBUG ? <CartDebugger /> : null}
-          </main>
-        </ReduxProvider>
+      <body>
+        <main className="min-h-screen max-w-7xl mx-auto px-4">{children}</main>
       </body>
     </html>
   );
