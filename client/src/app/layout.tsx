1145daa84c059d96cf26c9193b47c190d6385695
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-10-03 01:57:58 +0300

    updated

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -5,11 +5,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>
-        <main className="min-h-screen max-w-7xl mx-auto px-4">{children}</main>
-      </body>
-    </html>
-  );
+  return <html lang="en"></html>;
 }
