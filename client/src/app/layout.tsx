986316aaffff4f78cfd0d3a463df44d78344386a
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-10-04 11:58:15 +0300

    added navbar/footer

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,27 @@
+import Navbar from "@/components/Navbar";
 import "./global.css";
 import { ReduxProvider } from "@/redux/Provider";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "ShopSphere",
   description: "AI-powered E-commerce Platform",
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen flex flex-col bg-white text-gray-900">
+        <ReduxProvider>
+          <Navbar />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
