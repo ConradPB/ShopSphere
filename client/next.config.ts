a4ae4241638a851b085aae8aaaeae7b274cbb738
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-10-10 12:11:37 +0300

    updated

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "cdn.pixabay.com",
+      "res.cloudinary.com",
+      "picsum.photos",
+    ],
+  },
+
   async headers() {
     return process.env.NODE_ENV === "development"
       ? [
