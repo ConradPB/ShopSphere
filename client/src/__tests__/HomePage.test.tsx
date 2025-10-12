a9e8b9feec3733372cfec33944acb88f64cada43
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-10-12 13:49:38 +0300

    store import updated

--- client/src/__tests__/HomePage.test.tsx
+++ client/src/__tests__/HomePage.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import HomePage from "@/app/page";
 import * as productsLib from "@/lib/products";
 import { Provider } from "react-redux";
-import store from "@/redux/store";
+import { store } from "@/redux/store";
 
 jest.mock("@/lib/products");
 
