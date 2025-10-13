cf1ce405d63dbae19251f41aa91f5c2443037d6a
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-10-13 22:57:13 +0300

    imported act

--- client/src/__tests__/app/products/page.test.tsx
+++ client/src/__tests__/app/products/page.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import ProductsPage from "@/app/products/page";
 
 jest.mock("@/components/ProductGrid", () =>
