b9e8ff5ab9d4f42cadbb88dc899c6abe65cc5764
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-10-13 23:07:37 +0300

    updated

--- client/src/__tests__/app/products/page.test.tsx
+++ client/src/__tests__/app/products/page.test.tsx
@@ -1,5 +1,9 @@
-import { render, screen, act } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import ProductsPage from "@/app/products/page";
+
+jest.mock("@/components/ProductGrid", () =>
+  jest.fn(() => <div>Mocked Grid</div>)
+);
 
 describe("ProductsPage", () => {
   it("renders the products page content", async () => {
@@ -9,5 +13,10 @@ describe("ProductsPage", () => {
 
     // Updated to match your actual text
     expect(screen.getByText(/Explore Our Products/i)).toBeInTheDocument();
+  });
+
+  it("matches snapshot", () => {
+    const { asFragment } = render(<ProductsPage />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
