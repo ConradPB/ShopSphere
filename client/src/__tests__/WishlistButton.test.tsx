7f03fd0b5749cc542c4045f3e47d191718543677
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-10-13 10:55:23 +0300

    updated

--- client/src/__tests__/WishlistButton.test.tsx
+++ client/src/__tests__/WishlistButton.test.tsx
@@ -1,1 +1,18 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import WishlistButton from "@/components/ui/WishlistButton";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
+describe("WishlistButton", () => {
+  it("renders the wishlist button", () => {
+    render(
+      <Provider store={store}>
+        <WishlistButton productId="test-1" />
+      </Provider>
+    );
+
+    const btn = screen.getByRole("button");
+    expect(btn).toBeInTheDocument();
+  });
+});
