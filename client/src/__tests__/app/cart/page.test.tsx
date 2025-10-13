c8ee03533af425c623de8895c01a4d64fa0a7a8d
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-10-13 14:42:00 +0300

    added mockstore

--- client/src/__tests__/app/cart/page.test.tsx
+++ client/src/__tests__/app/cart/page.test.tsx
@@ -1,6 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Page from "@/app/cart/page";
 
+const mockStore = configureStore({
+  reducer: {
+    cart: cartReducer,
+    wishlist: wishlistReducer,
+  },
+});
 describe("Cart Page", () => {
   it("renders without crashing", () => {
     render(<Page />);
